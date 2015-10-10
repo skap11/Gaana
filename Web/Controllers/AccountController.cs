52bd349f8ad0239b23278714ffd551a22ef88ebd
skap11 <shivamkapoor287@gmail.com>
2015-10-10 19:05:20 +0530

    Initial setup
    Login using a form.

---
+++ Web/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        [HttpGet]
+        public ActionResult LogIn()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(UserAuthModel user)
+        {
+          return RedirectToAction("Index", "Home", user);
+        }
+    }
+}
---
+++ Web/Models/UserAuthModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class UserAuthModel
+    {
+        public string Username { get; set; }
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+        }
+        
+    }
+}
